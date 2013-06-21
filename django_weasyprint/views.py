8d11b6dc62bb45c0dce443e48bc8a45a6c0a10be
unknown <nicola@.penta.lan>
2013-06-21 10:57:59 +0200

    Add target parameter support to write_pdf()

--- django_weasyprint/views.py
+++ django_weasyprint/views.py
@@ -5,26 +5,46 @@ import weasyprint
 
 
 class PDFTemplateResponse(TemplateResponse):
-    def __init__(self, filename=None, *args, **kwargs):
+
+    """
+    absolute path of the target pdf file 
+    """
+    target = None
+
+    def __init__(self, filename=None, target=None, *args, **kwargs):
+        
         kwargs['content_type'] = "application/pdf"
         super(PDFTemplateResponse, self).__init__(*args, **kwargs)
         if filename:
             self['Content-Disposition'] = 'attachment; filename="%s"' % filename
         else:
             self['Content-Disposition'] = 'attachment'
+            
+        if target:
+            self.target = target 
 
     @property
     def rendered_content(self):
         """Returns the rendered pdf"""
         html = super(PDFTemplateResponse, self).rendered_content
         base_url = self._request.build_absolute_uri("/")
-        pdf = weasyprint.HTML(string=html, base_url=base_url).write_pdf()
-        return pdf
+        if self.target:
+            """
+            write the PDF to a file
+            """
+            weasyprint.HTML(string=html, base_url=base_url).write_pdf(target=self.target)
+            return None
+        else:
+            pdf = weasyprint.HTML(string=html, base_url=base_url).write_pdf()
+            return pdf
 
 
 class PDFTemplateResponseMixin(TemplateResponseMixin):
+    
     response_class = PDFTemplateResponse
     filename = None
+    target = None
+
 
     def get_filename(self):
         """
@@ -32,11 +52,18 @@ class PDFTemplateResponseMixin(TemplateResponseMixin):
         """
         return self.filename
 
+    def get_target(self):
+        """
+        Returns the target for the rendered PDF.
+        """
+        return self.target
+
     def render_to_response(self, *args, **kwargs):
         """
         Returns a response, giving the filename parameter to PDFTemplateResponse.
         """
         kwargs['filename'] = self.get_filename()
+        kwargs['target'] = self.get_target()
         return super(PDFTemplateResponseMixin, self).render_to_response(*args, **kwargs)
 
 
