42676a26ca9e83510d96ae1c068f0271c6881590
Florian Demmer <fdemmer@gmail.com>
2017-04-05 17:18:29 +0200

    Set version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-weasyprint',
-    version='0.2',
+    version='0.3',
     description='Django weasyprint app',
     url='https://github.com/fdemmer/django-weasyprint',
     author='Florian Demmer',
