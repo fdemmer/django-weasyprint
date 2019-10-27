f56ad5f4833954698ee0dae495661480263d6d26
Florian Demmer <fdemmer@gmail.com>
2019-10-27 11:08:14 +0100

    Add tests for views and django_url_fetcher

--- django_weasyprint/tests/test_views.py
+++ django_weasyprint/tests/test_views.py
@@ -1,22 +1,40 @@
-import os
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from django.test import SimpleTestCase, override_settings
+from django.test import SimpleTestCase
 
-from django_weasyprint.views import CONTENT_TYPE_PDF
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates')],
-    },
-]
+import django_weasyprint.tests  # noqa
+from django_weasyprint.views import CONTENT_TYPE_PDF, CONTENT_TYPE_PNG
 
 
-@override_settings(TEMPLATES=TEMPLATES, ROOT_URLCONF='django_weasyprint.tests.urls')
-class TestWeasyTemplateView(SimpleTestCase):
-
-    def test_get(self):
-        response = self.client.get('/pdf/')
-
+class WeasyTemplateViewTestCase(SimpleTestCase):
+    def test_get_html(self):
+        response = self.client.get('/html/')
         self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(sorted(response._headers.keys()), ['content-type'])
+        self.assertEqual(response['content-type'], 'text/html; charset=utf-8')
+        self.assertEqual(response.content, b'<h1>Example template</h1>\n')
+
+    def test_get_pdf(self):
+        response = self.client.get('/pdf/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(
+            sorted(response._headers.keys()),
+            ['content-disposition', 'content-type'],
+        )
         self.assertEqual(response['content-type'], CONTENT_TYPE_PDF)
+        self.assertEqual(
+            response['content-disposition'],
+            'attachment;filename="le-foo.pdf"',
+        )
+        self.assertEqual(response.content[:4], b'%PDF')
+
+    def test_get_png(self):
+        response = self.client.get('/png/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(sorted(response._headers.keys()), ['content-type'])
+        self.assertEqual(response['content-type'], CONTENT_TYPE_PNG)
+        self.assertEqual(response.content[1:4], b'PNG')
--- django_weasyprint/tests/urls.py
+++
@@ -1,8 +0,0 @@
-"""URLs for django_fido application tests."""
-from django.conf.urls import url
-
-from django_weasyprint.views import WeasyTemplateView
-
-urlpatterns = [
-    url(r'^pdf/$', WeasyTemplateView.as_view(template_name='example.html')),
-]
