3ac7978a5c8f26b96f4dd586d48ad5b8e66fd805
Florian Demmer <fdemmer@gmail.com>
2020-10-07 19:56:00 +0200

    Add Django 3.1 classifier

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
