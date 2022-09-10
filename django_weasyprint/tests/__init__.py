6655c33466a04ae165d5fba633eff8dbc4e783f1
Florian Demmer <fdemmer@gmail.com>
2022-09-10 13:10:09 +0200

    Improve test coverage

--- django_weasyprint/tests/__init__.py
+++ django_weasyprint/tests/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import django
 from django.conf import settings
 from django.core.management.utils import get_random_secret_key
+from django.templatetags.static import static
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -12,31 +13,13 @@ from django_weasyprint import WeasyTemplateResponseMixin, WeasyTemplateView
 logging.basicConfig(level=logging.DEBUG)
 
 
-class BaseView(TemplateView):
-    # simple Django CBV with template
-    template_name = 'example.html'
-
-
-class PDFDownloadView(WeasyTemplateResponseMixin, BaseView):
-    # set filename for download
-    pdf_filename = 'le-foo.pdf'
-
-
-class PDFView(WeasyTemplateView):
-    template_name = 'example.html'
-
-
-urlpatterns = [
-    path('html/', BaseView.as_view()),
-    path('pdf/', PDFView.as_view()),
-    path('pdf/download/', PDFDownloadView.as_view()),
-]
-
-
 settings.configure(
     SECRET_KEY=get_random_secret_key(),
     ALLOWED_HOSTS=['testserver'],
     ROOT_URLCONF=__name__,
+    INSTALLED_APPS=['django.contrib.staticfiles'],
+    STATIC_URL='/static/',
+    STATIC_ROOT='/www/static',
     MIDDLEWARE=[],
     TEMPLATES=[{
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -48,3 +31,26 @@ settings.configure(
     }},
 )
 django.setup()
+
+
+class BaseView(TemplateView):
+    # simple Django CBV with template
+    template_name = 'example.html'
+
+
+class PDFDownloadView(WeasyTemplateResponseMixin, BaseView):
+    # set filename for download
+    pdf_filename = 'le-foo.pdf'
+    # additional stylesheet(s)
+    pdf_stylesheets = [static('css/print.css')]
+
+
+class PDFView(WeasyTemplateView):
+    template_name = 'example.html'
+
+
+urlpatterns = [
+    path('html/', BaseView.as_view()),
+    path('pdf/', PDFView.as_view()),
+    path('pdf/download/', PDFDownloadView.as_view()),
+]
