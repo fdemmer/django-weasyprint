8e74ee7cde680aae90e31e12bfacc25a97f33296
crosspolar <18083323+crosspolar@users.noreply.github.com>
2026-03-28 10:07:44 +0100

    init

--- src/django_weasyprint/utils.py
+++ src/django_weasyprint/utils.py
@@ -11,7 +11,8 @@ from django.contrib.staticfiles.finders import find
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.files.storage import default_storage
 from django.urls import get_script_prefix
-
+from weasyprint import URLFetcher
+from weasyprint.urls import URLFetcherResponse
 
 log = logging.getLogger(__name__)
 
@@ -20,6 +21,50 @@ log = logging.getLogger(__name__)
 def get_reversed_hashed_files():
     return {v: k for k, v in staticfiles_storage.hashed_files.items()}
 
+class DjangoUrlFetcher(URLFetcher):
+    def fetch(self, url, headers=None):
+        if url.startswith("file:"):
+            log.debug("Attempt to fetch from %s", url)
+            mime_type, _ = mimetypes.guess_type(url)
+            url_path = urlparse(url).path
+            default_media_url = settings.MEDIA_URL in ("", get_script_prefix())
+            if not default_media_url and url_path.startswith(settings.MEDIA_URL):
+                log.debug('URL contains MEDIA_URL (%s)', settings.MEDIA_URL)
+                cleaned_media_root = str(settings.MEDIA_ROOT)
+                if not cleaned_media_root.endswith("/"):
+                    cleaned_media_root += "/"
+                absolute_path = url_path.replace(
+                    settings.MEDIA_URL, cleaned_media_root, 1
+                )
+                log.debug("Cleaned path: %s", absolute_path)
+                body = default_storage.open(absolute_path, "rb")
+                redirected_url = "file://" + absolute_path
+                return URLFetcherResponse(
+                    redirected_url, body, {"Content-Type": mime_type}
+                )
+
+            # path looks like a static file based on configured STATIC_URL
+            elif settings.STATIC_URL and url_path.startswith(settings.STATIC_URL):
+                log.debug("URL contains STATIC_URL (%s)", settings.STATIC_URL)
+                # strip the STATIC_URL prefix to get the relative filesystem path
+                relative_path = url_path.replace(settings.STATIC_URL, "", 1)
+                # detect hashed files storage and get path with un-hashed filename
+                if not settings.DEBUG and hasattr(staticfiles_storage, "hashed_files"):
+                    log.debug("Hashed static files storage detected")
+                    relative_path = get_reversed_hashed_files()[relative_path]
+                # find the absolute path using the static file finders
+                absolute_path = find(relative_path)
+                if absolute_path:
+                    log.debug("Loading static file: %s", absolute_path)
+                    body = open(absolute_path, "rb")
+                    redirected_url = "file://" + absolute_path
+                    return URLFetcherResponse(
+                        redirected_url, body, {"Content-Type": mime_type}
+                    )
+        # Fall back to weasyprint default fetcher for http/s: and file: paths
+        # that did not match MEDIA_URL or STATIC_URL.
+        log.debug('Forwarding to weasyprint.default_url_fetcher: %s', url)
+        return super().fetch(url, headers)
 
 def django_url_fetcher(url, *args, **kwargs):
     # attempt to load file:// paths to Django MEDIA or STATIC files directly from disk
