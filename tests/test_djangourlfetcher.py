cb12c96d6bd763d9379bd747ad441101fd24abc0
crosspolar <18083323+crosspolar@users.noreply.github.com>
2026-03-28 11:09:41 +0100

    working tests 2

--- tests/test_djangourlfetcher.py
+++ tests/test_djangourlfetcher.py
@@ -68,48 +68,48 @@ class URLFetcherTest(SimpleTestCase):
         mock_find.assert_called_once_with('css/styles.css')
         mock_open.assert_called_once_with('/www/static/css/styles.css', 'rb')
         self.assert_data(data, '/www/static/css/styles.css', 'text/css')
-    #
-    # @override_settings(
-    #     STATIC_URL='/static/',
-    #     STATIC_ROOT='/www/static',
-    #     STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage',  # noqa
-    #     STORAGES={
-    #         'staticfiles': {
-    #             'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',  # noqa
-    #         },
-    #     }
-    # )
-    # @mock.patch(
-    #     'django_weasyprint.utils.staticfiles_storage.hashed_files',
-    #     new_callable=mock.PropertyMock(return_value={
-    #         'css/styles.css': 'css/styles.60b250d16a6a.css',
-    #     }),
-    # )
-    # @mock.patch('django_weasyprint.utils.open')
-    # @mock.patch('django_weasyprint.utils.find',
-    # return_value='/www/static/css/styles.css')
-    # @mock.patch('weasyprint.default_url_fetcher')
-    # def test_manifest_static(self, mock_fetcher, mock_find, mock_open, hashed_files):
-    #     # request matches STATIC_URL, request handled
-    #     url = 'file:///static/css/styles.60b250d16a6a.css'
-    #     data = django_url_fetcher(url)
-    #     mock_fetcher.assert_not_called()
-    #     mock_find.assert_called_once_with('css/styles.css')
-    #     mock_open.assert_called_once_with('/www/static/css/styles.css', 'rb')
-    #     self.assert_data(data, '/www/static/css/styles.css', 'text/css')
-    #
-    #     mock_find.reset_mock()
-    #     mock_open.reset_mock()
-    #
-    #     # ManifestStaticFilesStorage.url() returns un-hashed URL with DEBUG=True,
-    #     # django_url_fetcher() is still able to find the file.
-    #     with override_settings(DEBUG=True):
-    #         data = django_url_fetcher('file:///static/css/styles.css')
-    #     mock_fetcher.assert_not_called()
-    #     mock_find.assert_called_once_with('css/styles.css')
-    #     mock_open.assert_called_once_with('/www/static/css/styles.css', 'rb')
-    #     self.assert_data(data, '/www/static/css/styles.css', 'text/css')
-    #
+
+    @override_settings(
+        STATIC_URL='/static/',
+        STATIC_ROOT='/www/static',
+        STATICFILES_STORAGE='django.contrib.staticfiles.storage.ManifestStaticFilesStorage',  # noqa
+        STORAGES={
+            'staticfiles': {
+                'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',  # noqa
+            },
+        }
+    )
+    @mock.patch(
+        'django_weasyprint.utils.staticfiles_storage.hashed_files',
+        new_callable=mock.PropertyMock(return_value={
+            'css/styles.css': 'css/styles.60b250d16a6a.css',
+        }),
+    )
+    @mock.patch('django_weasyprint.utils.open')
+    @mock.patch('django_weasyprint.utils.find',
+    return_value='/www/static/css/styles.css')
+    @mock.patch('weasyprint.URLFetcher')
+    def test_manifest_static(self, mock_fetcher, mock_find, mock_open, hashed_files):
+        # request matches STATIC_URL, request handled
+        url = 'file:///static/css/styles.60b250d16a6a.css'
+        data = DjangoUrlFetcher().fetch(url)
+        mock_fetcher.assert_not_called()
+        mock_find.assert_called_once_with('css/styles.css')
+        mock_open.assert_called_once_with('/www/static/css/styles.css', 'rb')
+        self.assert_data(data, '/www/static/css/styles.css', 'text/css')
+
+        mock_find.reset_mock()
+        mock_open.reset_mock()
+
+        # ManifestStaticFilesStorage.url() returns un-hashed URL with DEBUG=True,
+        # django_url_fetcher() is still able to find the file.
+        with override_settings(DEBUG=True):
+            data = DjangoUrlFetcher().fetch('file:///static/css/styles.css')
+        mock_fetcher.assert_not_called()
+        mock_find.assert_called_once_with('css/styles.css')
+        mock_open.assert_called_once_with('/www/static/css/styles.css', 'rb')
+        self.assert_data(data, '/www/static/css/styles.css', 'text/css')
+
     # @override_settings(STATIC_URL='/static/', STATIC_ROOT='/www/static')
     # @mock.patch('django_weasyprint.utils.open')
     # @mock.patch('django_weasyprint.utils.find', return_value=None)
